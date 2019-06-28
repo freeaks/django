f1a513f844ac24cd67da2f8343b994118af29e12
peter5502 <peter.parker5502@gmail.com>
2019-06-28 12:17:01 +0545

    second commit

--- signuppage/urls.py
+++ signuppage/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('ver/',views.verify,name='verify'),
+    path('doers/',views.doers,name='doers'),
+    path('givers/', views.givers, name='givers'),
     path('', views.index, name='index'),
 ]
--- signuppage/views.py
+++ signuppage/views.py
@@ -8,5 +8,15 @@ def index(request):
     return render(request, 'index.html', )
 
 
+def doers(request):
+    user4=Users()
+    user4.username=request.POST['user_name']
+    return render(request, 'doers.html',)
+
+
+def givers(request):
+    return render(request,'givers.html',)
+
+
 def verify(request):
     return HttpResponse('Your account is created, now go to your email to verify it or put the code on your mobile phone')
