36f0e9d037417a8dc85dce14104a1e505761ae81
peter5502 <peter.parker5502@gmail.com>
2019-07-01 21:01:58 +0545

    seventh

--- learning/urls.py
+++ learning/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', include('signuppage.urls')),
+    path('',include('signuppage.urls')),
 ]
